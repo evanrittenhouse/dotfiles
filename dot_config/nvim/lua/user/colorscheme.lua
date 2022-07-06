6726204467291775941a467c44fe1ac785537ba0
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-07-06 13:20:57 -0500

    minor changes

--- dot_config/nvim/lua/user/colorscheme.lua
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,6 @@
--- local colorscheme = 'gruvbox-material'
--- vim.api.nvim_set_var('gruvbox_material_background', 'hard')
-local colorscheme = 'kanagawa'
+local colorscheme = 'gruvbox-material'
+vim.api.nvim_set_var('gruvbox_material_background', 'hard')
+-- local colorscheme = 'kanagawa'
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
