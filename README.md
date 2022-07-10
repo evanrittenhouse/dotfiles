971f77462a5ee992980de7d1a50adb09cf7b31b9
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-07-09 21:31:31 -0500

    kanagawa

--- README.md
+++ README.md
@@ -3,7 +3,4 @@
 
 TODO
 
-1. install nvm.fish (and npm) and the rest of fisher packages
-2. have neovim automatically configure and install Python/JS+TS LSPs - also want neovim to automatically run PackerSync
-3. Figure out issue with tmux's $PATH on different loads - this seems like it may be solved with `tmux set-environment -g PATH $PATH`
-4. Figure out PATH differences between Ubuntu and macOS
+1. have neovim automatically configure and install Python/JS+TS LSPs - also want neovim to automatically run PackerSync
--- dot_config/nvim/lua/user/colorscheme.lua
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -1,9 +1,15 @@
-local colorscheme = 'gruvbox-material'
-vim.api.nvim_set_var('gruvbox_material_background', 'hard')
+-- local colorscheme = 'gruvbox-material'
+-- vim.api.nvim_set_var('gruvbox_material_background', 'hard')
 -- local colorscheme = 'kanagawa'
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found")
-  return
-end
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found")
+--   return
+-- end
+
+require('kanagawa').setup({
+  transparent = true,
+})
+
+vim.cmd [[colorscheme kanagawa]]
