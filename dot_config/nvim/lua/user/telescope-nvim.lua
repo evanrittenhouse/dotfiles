0f4cef9386d7ae94bbbf741e3a0983fdb3d627a1
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-07-12 19:00:31 -0500

    lightspeed omnidirectional seraching

--- dot_config/nvim/lua/user/telescope-nvim.lua
+++
@@ -1,7 +0,0 @@
-local keymap = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
-
-keymap('n', '<leader>ff', '<cmd>Telescope git_files<cr>', opts)
-keymap('n', '<leader>FF', '<cmd>Telescope find_files<cr>', opts)
-keymap('n', '<leader>rg', '<cmd>Telescope live_grep<cr>', opts)
-keymap('n', '<leader>gc', '<cmd>Telescope git_commits<cr>', opts)
