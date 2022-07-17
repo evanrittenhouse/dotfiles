53fe55adfec246eed2b014c1faf7de37e9f7a263
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-07-17 18:50:28 -0500

    kanagawa and gruvbox alacritty themes
    change gruvbox nvim to transparent background, matching kanagawa

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -1,6 +1,6 @@
 require('kanagawa').setup({
   transparent = true,
 })
-vim.api.nvim_set_var('gruvbox_material_background', 'hard')
+vim.api.nvim_set_var('gruvbox_material_transparent_background', 1)
 
-vim.cmd('colorscheme kanagawa')
+vim.cmd('colorscheme gruvbox-material')
