f32336113baa29a0ce89e04622658cbdf8c80560
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-11-27 17:11:20 -0600

    Add batter, volume, wireless network, and top bar to Polybar

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -5,6 +5,7 @@ killall -q polybar
 
 # launch new bar 
 echo "----" | tee -a /tmp/polybar1.log
-polybar mybar --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bottom --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_bottom.log & disown
+polybar top --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_top.log & disown
 
 echo "Polybar launched..."
