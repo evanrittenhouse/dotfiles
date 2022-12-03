38a922f11e07e87b1b3eea391c0c10fa29655f16
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-12-03 10:52:43 -0600

    (polybar) display bar on vairable monitor
    still need to show it on the larger monitor

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -8,4 +8,16 @@ echo "----" | tee -a /tmp/polybar1.log
 polybar bottom --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_bottom.log & disown
 polybar top --config=$HOME/.config/polybar/config.ini 2>&1 | tee -a /tmp/polybar_top.log & disown
 
+if type "xrandr";
+then
+        for m in $(xrandr --query | grep " connected" | cut -d" " -fi); do
+                MONITOR=$m 
+                polybar --reload top & 
+                polybar --reload bottom & 
+        done
+else
+        polybar --reload top &
+        polybar --reload bottom & 
+fi
+
 echo "Polybar launched..."
