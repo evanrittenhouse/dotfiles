346c2d24317cbbf8ed2e79f05c438c7e6daff771
Evan Rittenhouse <evanrittenhouse@gmail.com>
2022-12-24 01:38:34 -0600

    (nvim) switch telescope \ff picker

--- dot_config/nvim/lua/plugin_configs/telescope.lua
+++ dot_config/nvim/lua/plugin_configs/telescope.lua
@@ -15,8 +15,8 @@ telescope.load_extension('file_browser')
 
 local keymap = require('utils').keymap
 local opts = require('utils').keymap_opts
-keymap('n', '<leader>ff', '<cmd>Telescope git_files<cr>', opts)
-keymap('n', '<leader>FF', '<cmd>Telescope find_files<cr>', opts)
+keymap('n', '<leader>ff', '<cmd>Telescope find_files<cr>', opts)
+keymap('n', '<leader>FF', '<cmd>Telescope git_files<cr>', opts)
 keymap('n', '<leader>rg', '<cmd>Telescope live_grep<cr>', opts)
 keymap('n', '<leader>fb', '<cmd>Telescope file_browser<cr>', opts)
 keymap('n', '<leader>fo', '<cmd>Telescope oldfiles cwd_only=True<cr>', opts)
