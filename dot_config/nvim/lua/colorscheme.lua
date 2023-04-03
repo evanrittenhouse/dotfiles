bb28c0fe8906863ae93de7f9516a764c9a9ec371
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-04-03 09:03:33 -0500

    (neovim) colorscheme

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -1,6 +1,3 @@
-require('kanagawa').setup({
-  transparent = true,
-})
 -- vim.api.nvim_set_var('gruvbox_material_transparent_background', 1)
 
 require('onedark').setup {
@@ -11,4 +8,7 @@ require('onedark').setup {
   }
 }
 
+require('kanagawa').setup({
+  transparent = true
+})
 vim.cmd('colorscheme kanagawa')
