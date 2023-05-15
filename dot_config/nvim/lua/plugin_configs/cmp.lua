b160bc901a2d456e1f49849ee9b6fc83dad5bcc0
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-05-14 19:50:28 -0500

    (font) upgrade glyph codes for new material design codes

--- dot_config/nvim/lua/plugin_configs/cmp.lua
+++ dot_config/nvim/lua/plugin_configs/cmp.lua
@@ -17,13 +17,13 @@ end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "",
+  Text = "󰉿",
   Method = "m",
-  Function = "",
+  Function = "󰊕",
   Constructor = "",
   Field = "",
-  Variable = "",
-  Class = "",
+  Variable = "x",
+  Class = "",
   Interface = "",
   Module = "",
   Property = "",
@@ -32,16 +32,16 @@ local kind_icons = {
   Enum = "",
   Keyword = "",
   Snippet = "",
-  Color = "",
-  File = "",
+  Color = "",
+  File = "",
   Reference = "",
-  Folder = "",
+  Folder = "",
   EnumMember = "",
-  Constant = "",
+  Constant = "",
   Struct = "",
   Event = "",
-  Operator = "",
-  TypeParameter = "",
+  Operator = "",
+  TypeParameter = "󰊄"
 }
 
 cmp.setup {
