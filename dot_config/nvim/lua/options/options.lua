326f7dadf6d07308c1b67b62fbf661ce4958702b
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-06-12 21:37:44 -0500

    (neovim) add set_vim_option to utils

--- dot_config/nvim/lua/options/options.lua
+++ dot_config/nvim/lua/options/options.lua
@@ -1,21 +1,22 @@
-vim.opt.completeopt = { "menuone", "noselect" }
-vim.opt.ignorecase = true
-vim.opt.showmode = false
-vim.opt.splitright = true
-vim.opt.wrap = false
-vim.opt.number = true
-vim.opt.expandtab = true
-vim.opt.showmode = false
-vim.opt.undofile = true
-vim.o.updatetime = 300
-vim.opt.incsearch = true
-vim.g.mapleader = " " -- change leader key
+local set_vim_option = require "utils".set_vim_option
 
+set_vim_option('completeopt', { "menuone", "noselect" })
+set_vim_option('ignorecase', true)
+set_vim_option('showmode', false)
+set_vim_option('splitright', true)
+set_vim_option('wrap', false)
+set_vim_option('number', true)
+set_vim_option('expandtab', true)
+set_vim_option('showmode', false)
+set_vim_option('undofile', true)
+set_vim_option('incsearch', true)
 -- Make searches case-insensitive, unless you include a capital letter in the search pattern
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+set_vim_option('ignorecase', true)
+set_vim_option('smartcase', true)
 
+vim.o.updatetime = 300
+vim.g.mapleader = " " -- change leader key
 -- vim.cmd('cabbrev h vert h')
 
--- necessary for feline
+-- Necessary for feline
 vim.cmd('set termguicolors')
--- dot_config/nvim/lua/utils.lua
+++ dot_config/nvim/lua/utils.lua
@@ -3,4 +3,8 @@ local utils = {}
 utils.keymap = vim.api.nvim_set_keymap
 utils.keymap_opts = { noremap = true, silent = true }
 
+utils.set_vim_option = function(option, arg)
+  vim.opt[option] = arg
+end
+
 return utils
