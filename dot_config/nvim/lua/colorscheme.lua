e27d91bf97f9bc8bf04aba70efa4441bc4b0dc4a
Evan Rittenhouse <evanrittenhouse@gmail.com>
2023-06-29 09:10:36 -0500

    (neovim) remove line gutter background

--- dot_config/nvim/lua/colorscheme.lua
+++ dot_config/nvim/lua/colorscheme.lua
@@ -1,6 +1,16 @@
 -- vim.api.nvim_set_var('gruvbox_material_transparent_background', 1)
 
 require('kanagawa').setup({
-  transparent = true
+  transparent = true,
+  colors = {
+    theme = {
+      all = {
+        ui = {
+          bg_gutter = "none"
+        }
+      }
+    }
+  }
 })
+
 vim.cmd('colorscheme kanagawa')
