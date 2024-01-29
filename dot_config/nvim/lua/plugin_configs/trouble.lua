d80266577d344acd6f8059d16f8c1b51f4d8d672
Evan Rittenhouse <evanrittenhouse@gmail.com>
2024-01-28 22:29:59 -0600

    (nvim) add TroubleClose alias

--- dot_config/nvim/lua/plugin_configs/trouble.lua
+++ dot_config/nvim/lua/plugin_configs/trouble.lua
@@ -4,3 +4,4 @@ local opts = require('utils').keymap_opts
 require('trouble').setup()
 keymap('n', '<Leader>td', ':TroubleToggle document_diagnostics<CR>', opts)
 keymap('n', '<Leader>tw', ':TroubleToggle workspace_diagnostics<CR>', opts)
+keymap('n', '<Leader>tc', ':TroubleClose<CR>', opts)
