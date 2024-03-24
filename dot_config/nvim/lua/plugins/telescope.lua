7481b607610a69c26dc82a87e7393e48342f07ce
Evan Rittenhouse <evanrittenhouse@gmail.com>
2024-03-24 15:58:05 -0500

    (nvim) replace ripgrep searcher with ripgrep w/ args

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -1,16 +1,22 @@
 local M = {
   "nvim-telescope/telescope.nvim",
-  event = "BufReadPre",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope-fzf-native.nvim",
+    "nvim-telescope/telescope-live-grep-args.nvim",
     "burntsushi/ripgrep",
     "kyazdani42/nvim-web-devicons",
   },
+  config = function(_, opts)
+    local telescope = require("telescope")
+
+    telescope.setup(opts)
+    telescope.load_extension("live_grep_args")
+  end,
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<CR>" },
     { "<leader>FF", "<cmd>Telescope git_files<cr>" },
-    { "<leader>rg", "<cmd>Telescope live_grep<cr>" },
+    { "<leader>rg", "<cmd>lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>" },
     { "<leader>fo", "<cmd>Telescope oldfiles cwd_only=True<cr>" },
     { "<leader>tr", "<cmd>Telescope resume<cr>" },
     { "<leader>ch", "<cmd>Telescope command_history<cr>" }
