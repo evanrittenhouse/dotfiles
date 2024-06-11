31bb071c3c37c7b042205b8203aa06d81d247ffb
Evan Rittenhouse <erittenhouse@cloudflare.com>
2024-06-11 10:45:26 -0500

    (nvim) update trouble with breaking changes

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,10 +1,39 @@
 local M = {
   "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
   keys = {
-    { "<Leader>td", ":TroubleToggle document_diagnostics<CR>" },
-    { "<Leader>tw", ":TroubleToggle workspace_diagnostics<CR>" },
-    { "<Leader>tc", ":TroubleClose<CR>" }
-  }
+    {
+      "<leader>tw",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle focus=false win.postition=left<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble lsp toggle focus=false win.position=left<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>tL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>tQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
 
 return M
