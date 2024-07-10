15df01be229078d7fe1e0b58e6051ba56e152e96
Evan Rittenhouse <evanrittenhouse@gmail.com>
2024-07-09 21:51:42 -0500

    (nvim) add primagen key remaps

--- dot_config/nvim/lua/vim_config/keybinds.lua
+++ dot_config/nvim/lua/vim_config/keybinds.lua
@@ -14,3 +14,19 @@ keymap('n', 'N', 'Nzz', opts)
 
 -- clear search highlight when you hit escape
 keymap('n', '<ESC>', ':nohlsearch<Bar>:echo<CR>', opts)
+
+-- move highlighted lines together
+keymap('v', 'J', ":m '>+1<CR>gv=gv", opts)
+keymap('v', 'K', ":m '<-2<CR>gv=gv", opts)
+
+-- keep cursor in current position when appending with `J`
+keymap("n", "J", "mzJ`z", opts)
+
+-- Center <C-d>, <C-u>
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- Copy to system clipboard
+keymap("n", "<leader>y", "\"+y", opts)
+keymap("n", "<leader>Y", "\"+Y", opts)
+keymap("v", "<leader>y", "\"+y", opts)
